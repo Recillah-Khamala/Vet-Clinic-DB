a18331fbffe92d7c3a0b5f4f8fa8907b8a351b0a
recillah khamala <recillahk@gmail.com>
2022-12-08 02:27:38 +0300

    Add the sql used to create animal's table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE IF NOT EXISTS animals (
+    id BIGSERIAL, 
+    name VARCHAR(100),
+    date_of_birth DATE, 
+    escape_attempts INT, 
+    neutered BOOLEAN, 
+    weight_kg DECIMAL
 );
