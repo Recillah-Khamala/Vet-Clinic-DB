2af70969f7527b71a4f3ba57f8f652a12f70f795
recillah khamala <recillahk@gmail.com>
2022-12-09 18:18:29 +0300

    Create species table and add columns

--- schema.sql
+++ schema.sql
@@ -9,12 +9,22 @@ CREATE TABLE IF NOT EXISTS animals (
     weight_kg DECIMAL
 );
 
+-- Day 2
+
 ALTER TABLE animals
 ADD species VARCHAR(100);
+
+-- Day 3
 
 CREATE TABLE IF NOT EXISTS owners(
 	id INT GENERATED ALWAYS AS IDENTITY,
 	full_name VARCHAR(255),
 	age INT,
 	PRIMARY KEY (id)
+);
+
+CREATE TABLE species (
+    id BIGSERIAL,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
 );
