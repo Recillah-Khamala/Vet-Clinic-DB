16844c4ffcfec54a5d8ade182632c63a3f610cf9
recillah khamala <recillahk@gmail.com>
2022-12-13 11:35:24 +0300

    Add sql for creating table and populating table

--- schema.sql
+++ schema.sql
@@ -59,3 +59,17 @@ ALTER TABLE animals
 ADD CONSTRAINT fk_owner_id
 FOREIGN KEY (owner_id)
 REFERENCES owners(id);
+
+
+/*============= Day 5 =============*/
+
+-- Make sure that you have your database set up with the schema and data from your previous projects.
+DROP TABLE visits;
+
+CREATE TABLE visits(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  animal_id INT REFERENCES animals(id),
+  vet_id INT REFERENCES vets(id),
+  date_of_visit DATE,
+  PRIMARY KEY(id)
+);
